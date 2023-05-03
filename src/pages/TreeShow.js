5d032fbf0565e840a435f2aad252c3fa2532712b
sam-saenz <samsaenz31@gmail.com>
2023-05-03 16:02:01 -0700

    Finished read functionality stories

--- src/pages/TreeShow.js
+++ src/pages/TreeShow.js
@@ -1,8 +1,25 @@
-const TreeShow = () => {
+import { useParams } from "react-router-dom"
+import { Container } from "reactstrap"
+
+const TreeShow = ({ trees }) => {
+    const { id } = useParams()
+    console.log(id);
+    let currentTree = trees?.find((tree) => tree.id === +id)
+    console.log(currentTree);
+
+
     return (
-        <>
-            <h3>TreeShow</h3>
-        </>
+        <main>
+            <Container
+                className="show-profile"
+                fluid="sm"
+            >
+                <h1>{currentTree.name}, Age: {currentTree.age}</h1>
+                <img src={currentTree.image} alt={`${currentTree.name}'s profile picture`} />
+                <p>{currentTree.enjoys}</p>
+            </Container>
+
+        </main>
     )
 }
 
