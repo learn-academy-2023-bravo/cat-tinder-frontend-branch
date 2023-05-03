382fc3b214e692619aefbc72fe24e89400e3a66c
raymondkneipp <raymond.f.kneipp@gmail.com>
2023-05-03 16:19:05 -0700

    add background image to tree show

--- src/pages/TreeShow.js
+++ src/pages/TreeShow.js
@@ -1,26 +1,33 @@
-import { useParams } from "react-router-dom"
-import { Container } from "reactstrap"
+import { useParams } from 'react-router-dom'
+import { Container } from 'reactstrap'
 
 const TreeShow = ({ trees }) => {
-    const { id } = useParams()
-    console.log(id);
-    let currentTree = trees?.find((tree) => tree.id === +id)
-    console.log(currentTree);
+  const { id } = useParams()
+  console.log(id)
+  let currentTree = trees?.find((tree) => tree.id === +id)
+  console.log(currentTree)
 
-
-    return (
-        <main>
-            <Container
-                className="show-profile"
-                fluid="sm"
-            >
-                <h1>{currentTree.name}, Age: {currentTree.age}</h1>
-                <img src={currentTree.image} alt={`${currentTree.name}'s profile picture`} />
-                <p>{currentTree.enjoys}</p>
-            </Container>
-
-        </main>
-    )
+  return (
+    <main>
+      <Container className='show-profile' fluid='sm'>
+        <h1>
+          {currentTree.name}, Age: {currentTree.age}
+        </h1>
+        <div className='show-container'>
+          <div
+            className='show-background'
+            style={{ backgroundImage: `url(${currentTree.image})` }}
+          />
+          <img
+            className='show-image'
+            src={currentTree.image}
+            alt={`${currentTree.name}'s profile picture`}
+          />
+        </div>
+        <p>{currentTree.enjoys}</p>
+      </Container>
+    </main>
+  )
 }
 
 export default TreeShow
