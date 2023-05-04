8e5346ff1cf7c1c394f59d4ed4db51769f91cb59
sam-saenz <samsaenz31@gmail.com>
2023-05-04 14:30:22 -0700

    Styled some pages

--- src/pages/TreeShow.js
+++ src/pages/TreeShow.js
@@ -1,36 +1,50 @@
 import { NavLink, useParams } from 'react-router-dom'
 import { Button } from 'reactstrap'
 
-const TreeShow = ({ trees }) => {
-  const { id } = useParams()
-  console.log(id)
-  let currentTree = trees?.find((tree) => tree.id === +id)
-  console.log(currentTree)
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 
-  return (
-    <div>
-      <div className='show-profile'>
-        <h1>
-          {currentTree.name}, Age: {currentTree.age}
-        </h1>
-        <div className='show-container'>
-          <div
-            className='show-background'
-            style={{ backgroundImage: `url(${currentTree.image})` }}
-          />
-          <img
-            className='show-image'
-            src={currentTree.image}
-            alt={`${currentTree.name}'s profile picture`}
-          />
+const TreeShow = ({ trees }) => {
+    const { id } = useParams()
+    console.log(id)
+    let currentTree = trees?.find((tree) => tree.id === +id)
+    console.log(currentTree)
+
+    const index = trees.findIndex(tree => tree.id === +id)
+    const next = trees[index + 1 > trees.length - 1 ? 0 : index + 1].id
+    const previous = trees[index === 0 ? trees.length - 1 : index - 1].id
+
+    return (
+        <div>
+            <div className='show-profile'>
+                <div className='show-top'>
+                    <Button color='secondary' to={`/treeshow/${previous}`} tag={NavLink} outline={true}>
+                        Previous
+                    </Button>
+                    <h1>
+                        {currentTree.name}, Age: {currentTree.age}
+                    </h1>
+                    <Button color='secondary' to={`/treeshow/${next}`} tag={NavLink} outline={true}>
+                        Next
+                    </Button>
+                </div>
+                <div className='show-container'>
+                    <div
+                        className='show-background'
+                        style={{ backgroundImage: `url(${currentTree.image})` }}
+                    />
+                    <img
+                        className='show-image'
+                        src={currentTree.image}
+                        alt={`${currentTree.name}'s profile picture`}
+                    />
+                </div>
+                <p>{currentTree.enjoys}</p>
+            </div>
+            <Button color='secondary' to='/treeindex' tag={NavLink} outline={true}>
+                Back
+            </Button>
         </div>
-        <p>{currentTree.enjoys}</p>
-      </div>
-      <Button color='secondary' to='/treeindex' tag={NavLink} outline={true}>
-        Back
-      </Button>
-    </div>
-  )
+    )
 }
 
 export default TreeShow
