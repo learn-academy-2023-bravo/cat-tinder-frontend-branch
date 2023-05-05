3946a5ea0030cd9488948b90b63a430b9a795b72
raymondkneipp <raymond.f.kneipp@gmail.com>
2023-05-05 11:31:05 -0700

    add error handling to preview component

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,20 +1,33 @@
+import React, { useState, useEffect } from 'react'
+
 const Preview = ({ source }) => {
-    function validImage(url) {
-        // console.log(url);
-        return fetch(url, { method: 'HEAD' }).then(res => {
-            // console.log(res.headers.get('Content-Type'));
-            return res.headers.get('Content-Type').startsWith('image')
-        })
+  const [isValidImage, setIsValidImage] = useState(null)
+
+  useEffect(() => {
+    async function checkImageValidity() {
+      try {
+        const response = await fetch(source, { method: 'HEAD' })
+        const contentType = response.headers.get('Content-Type')
+        setIsValidImage(contentType.startsWith('image'))
+      } catch (e) {
+        console.log(e)
+
+        setIsValidImage(false)
+      }
     }
-    console.log(validImage(source));
-    // if (validImage(source)) {
-    //     return (
-    //         <img className='preview' src={source} />
-    //     )
-    // }
-    // return (
-    //     <span>Invalid image URL: {source}</span>
-    // )
+
+    checkImageValidity()
+  }, [source])
+
+  if (isValidImage === null) {
+    return <span>Loading...</span>
+  }
+
+  if (isValidImage) {
+    return <img className='preview' src={source} />
+  }
+
+  return <span>Invalid image URL</span>
 }
 
 export default Preview
